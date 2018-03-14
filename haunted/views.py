a75039e07ae58c0e5a31e28a947f68c166d545f8
Huub Bouma <bouma@w20e.com>
2018-03-14 16:20:37 +0100

    wip render model

--- haunted/views.py
+++ haunted/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    pass
-
+    return render(request, 'haunted/index.html', {})
--- src/index.js
+++ src/index.js
@@ -2,3 +2,151 @@ import screenshot from 'screenshot'
 
 console.log(screenshot)
 screenshot().then(img => console.log("OOOO Ah screenshot", img))
+
+var THREE = require('three');
+import OBJLoader from 'three-obj-loader'
+OBJLoader(THREE)
+
+document.addEventListener('DOMContentLoaded', main);
+
+function main() {
+    init();
+    animate();
+}
+var container;
+
+var camera, scene, renderer, bgTexture, bgWidth, bgHeight;
+
+var mouseX = 0, mouseY = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+
+function init() {
+
+    container = document.createElement( 'div' );
+    document.body.appendChild( container );
+
+    camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+    camera.position.z = 250;
+
+    // scene
+
+    scene = new THREE.Scene();
+
+    var loader = new THREE.TextureLoader();
+    loader.setCrossOrigin("");
+
+    bgTexture = loader.load("https://raw.githubusercontent.com/Rabbid76/graphics-snippets/master/resource/texture/background.jpg",
+        function ( texture ) {
+            var img = texture.image;
+            bgWidth= img.width;
+            bgHeight = img.height;
+        }
+    );
+    scene.background = bgTexture;
+    bgTexture.wrapS = THREE.MirroredRepeatWrapping;
+    bgTexture.wrapT = THREE.MirroredRepeatWrapping;
+
+    var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+    scene.add( ambientLight );
+
+    var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
+    camera.add( pointLight );
+    scene.add( camera );
+
+    // texture
+
+    var manager = new THREE.LoadingManager();
+    manager.onProgress = function ( item, loaded, total ) {
+
+	console.log( item, loaded, total );
+
+    };
+
+    var textureLoader = new THREE.TextureLoader( manager );
+    var texture = textureLoader.load( '/static/haunted/wagtail_screenshot.png' );
+
+    // model
+
+    var onProgress = function ( xhr ) {
+	if ( xhr.lengthComputable ) {
+	    var percentComplete = xhr.loaded / xhr.total * 100;
+	    console.log( Math.round(percentComplete, 2) + '% downloaded' );
+	}
+    };
+
+    var onError = function ( xhr ) {
+    };
+
+    var objLoader = new THREE.OBJLoader( manager );
+
+    objLoader.load( '/static/haunted/male02.obj', function ( object ) {
+
+	object.traverse( function ( child ) {
+
+	    if ( child instanceof THREE.Mesh ) {
+
+		child.material.map = texture;
+
+	    }
+
+	} );
+
+	object.position.y = - 95;
+	scene.add( object );
+
+    }, onProgress, onError );
+
+    //
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    container.appendChild( renderer.domElement );
+
+    document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+    window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+    windowHalfX = window.innerWidth / 2;
+    windowHalfY = window.innerHeight / 2;
+
+    var aspect = window.innerWidth / window.innerHeight;
+
+    camera.aspect = aspect;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove( event ) {
+
+    mouseX = ( event.clientX - windowHalfX ) / 2;
+    mouseY = ( event.clientY - windowHalfY ) / 2;
+
+}
+
+
+function animate() {
+
+    requestAnimationFrame( animate );
+    render();
+
+}
+
+function render() {
+
+    camera.position.x += ( mouseX - camera.position.x ) * .05;
+    camera.position.y += ( - mouseY - camera.position.y ) * .05;
+
+    camera.lookAt( scene.position );
+
+    renderer.render( scene, camera );
+
+}
